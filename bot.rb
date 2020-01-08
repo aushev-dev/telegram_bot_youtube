8d1502e625b4cb0e47f523eb9019f63e0075027f
mairbekaushev <Mair>
2020-01-08 16:05:22 +0300

    Update add rescue

--- bot.rb
+++ bot.rb
@@ -4,12 +4,16 @@ token = '907693429:AAGwtga9dT_2AIj2Km8RrT4OffypCLeZPGI'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-  	when /youtu/
-  		%x(youtube-dl -x --audio-format mp3 --output "audio.%(ext)s" #{message.text})
-		bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/Users/mairbekaushev/rails/telegram-bot2/audio.mp3', 'audio/mp3'))
-	when //
-		bot.api.send_message(chat_id: 64819429, text: message.text)
-    end
+    begin
+	    case message.text
+	  	when /youtu/
+	  		%x(youtube-dl -x --audio-format mp3 --output "audio.%(ext)s" #{message.text})
+			bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/Users/mairbekaushev/rails/telegram-bot2/audio.mp3', 'audio/mp3'))
+		when //
+			bot.api.send_message(chat_id: 64819429, text: message.text)
+	    end
+	 rescue
+	 	bot.api.send_message(chat_id: message.chat.id, text: "Sorry, #{message.from.first_name}, it's error")
+	 end
   end
 end
