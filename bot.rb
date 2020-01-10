b79e95146fb854e9fb5fc8997a60f656ae36b1ac
mairbekaushev <Mair>
2020-01-10 18:43:29 +0300

    add all users permisson

--- bot.rb
+++ bot.rb
@@ -6,7 +6,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     begin
 	    case message.text
-	  	when /youtube/
+	  	when /youtu/
 	  		%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
 			bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
 		when //
