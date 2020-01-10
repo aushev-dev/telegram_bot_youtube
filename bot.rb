72427b8882fe9c483d67376b1eb6fbf989373fda
mairbekaushev <Mair>
2020-01-10 10:07:34 +0300

    will work

--- bot.rb
+++ bot.rb
@@ -8,7 +8,7 @@ Telegram::Bot::Client.run(token) do |bot|
 	    case message.text
 	  	when /youtu/
 	  		%x(youtube-dl -x --audio-format mp3 --output "audio.%(ext)s" #{message.text})
-			bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/Users/mairbekaushev/rails/telegram-bot2/audio.mp3', 'audio/mp3'))
+			bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/Users/mairbekaushev/rails/backup/telegram-bot2/audio.mp3', 'audio/mp3'))
 		when //
 			bot.api.send_message(chat_id: 64819429, text: message.text)
 	    end
