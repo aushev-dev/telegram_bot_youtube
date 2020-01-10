b6e406483d94e5abba3b370f531c10dc18aa31d6
mairbekaushev <Mair>
2020-01-10 10:25:43 +0300

    will work 3

---
+++ bot.rb
@@ -0,0 +1,19 @@
+require 'telegram/bot'
+
+token = '907693429:AAGwtga9dT_2AIj2Km8RrT4OffypCLeZPGI'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    begin
+	    case message.text
+	  	when /youtube/
+	  		%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
+			bot.api.send_audio(chat_id: 64819429, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
+		when //
+			bot.api.send_message(chat_id: 64819429, text: message.text)
+	    end
+	 rescue
+	 	bot.api.send_message(chat_id: message.chat.id, text: "Sorry, #{message.from.first_name}, it's error")
+	 end
+  end
+end
