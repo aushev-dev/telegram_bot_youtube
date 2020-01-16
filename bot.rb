09ee106d575a67dc14f3fd8150242662dd5004c6
mairbekaushev <Mair>
2020-01-16 22:59:44 +0300

    add acces error2

--- bot.rb
+++ bot.rb
@@ -12,7 +12,7 @@ Telegram::Bot::Client.run(token) do |bot|
 	end
 
     begin
-    	return ban(bann) unless message.chat.id == 64819429 
+    	if message.chat.id == 64819429 
 		    case message.text
 		  	when /youtu/
 		  		%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
@@ -22,6 +22,9 @@ Telegram::Bot::Client.run(token) do |bot|
 		    end 	
 	 rescue
 	 	bot.api.send_message(chat_id: message.chat.id, text: "Error")
+	 else
+	 	bot.api.send_message(chat_id: message.chat.id, text: "Извини, #{message.from.first_name}, у тебя нет доступа")
+
 	 end
   end
 end
