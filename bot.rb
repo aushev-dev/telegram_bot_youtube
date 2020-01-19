f62e5dae85b70bbc7318ca7cd54a57150bbb23e9
mairbekaushev <Mair>
2020-01-19 17:10:05 +0300

    add acces error6

--- bot.rb
+++ bot.rb
@@ -14,11 +14,11 @@ Telegram::Bot::Client.run(token) do |bot|
     begin
     	return ban(bann) unless message.chat.id == 64819429 
 		    case message.text
-			  	when /youtu/
-			  		%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
-					bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
-				when //
-					bot.api.send_message(chat_id: message.chat.id, text: message.text)
+		  	when /youtu/
+		  		%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
+				bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
+			when //
+				bot.api.send_message(chat_id: message.chat.id, text: message.text)
 		    end 	
 	 rescue
 	 	bot.api.send_message(chat_id: message.chat.id, text: "Error")
