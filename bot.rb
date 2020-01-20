82ab5b550ddd85be43fe80fb43f9435408b37210
mairbekaushev <Mair>
2020-01-20 06:57:58 +0300

    change quality

--- bot.rb
+++ bot.rb
@@ -15,7 +15,7 @@ Telegram::Bot::Client.run(token) do |bot|
     	return ban(bann) unless message.chat.id == 64819429 
 		    case message.text
 		  	when /youtu/
-		  		%x(/app/youtube-dl.dms -x --audio-format mp3 --audio-quality 5 --output "/tmp/audio.%(ext)s" #{message.text})
+		  		%x(/app/youtube-dl.dms -x --audio-format mp3 --audio-quality 8 --output "/tmp/audio.%(ext)s" #{message.text})
 				bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
 			when //
 				bot.api.send_message(chat_id: message.chat.id, text: message.text)
