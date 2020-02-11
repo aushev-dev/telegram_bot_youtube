4d1f668708a44280c3faf8ef842d80f7084d3840
mairbekaushev <Mair>
2020-02-11 23:59:36 +0300

    test

--- bot.rb
+++ bot.rb
@@ -6,7 +6,7 @@ token = '907693429:AAGwtga9dT_2AIj2Km8RrT4OffypCLeZPGI'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-
+=begin
   	def ban bann
 	bot.api.send_message(chat_id: message.chat.id, text: "Извини, #{message.from.first_name}, у тебя нет доступа")
 	end
@@ -25,5 +25,20 @@ Telegram::Bot::Client.run(token) do |bot|
 	 end
   end
 end
+=end
+begin
+if message.chat.id == 64819429 
+	case message.text
+		when /youtu/
+			%x(/app/youtube-dl.dms -x --audio-format mp3 --audio-quality 8 --output "/tmp/audio.%(ext)s" #{message.text})
+			bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
+		when //
+			bot.api.send_message(chat_id: message.chat.id, text: message.text)
+	end 	
 
-
+else 
+	bot.api.send_message(chat_id: message.chat.id, text: "Извини, #{message.from.first_name}, у тебя нет доступа")
+end
+	 rescue
+	 	bot.api.send_message(chat_id: message.chat.id, text: "Error")
+end
