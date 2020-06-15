f350090a9b2bda55cccb7fe8510a3e9d2e484a03
Mair <aushev92@gmail.com>
2020-06-15 13:44:44 +0300

    refactoring

--- bot.rb
+++ bot.rb
@@ -1,20 +1,20 @@
 require 'telegram/bot'
 
 token = ENV['TOKEN']
-
+white_list = ENV['WL'].splite(',')
 
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
 		begin
-			if message.chat.id == 64819429 ||  message.chat.id == 608598294 || message.chat.id == 1145075672
+			if white_list.include?(message.chat.id)
 				case message.text
 				when /youtu/
 					%x(/app/youtube-dl.dms -x --audio-format mp3 --audio-quality 8 --output "/tmp/audio.%(ext)s" #{message.text})
 					bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
 				when //
 					bot.api.send_message(chat_id: message.chat.id, text: message.text)
-				end 	
+				end
 			end
 		rescue
 			bot.api.send_message(chat_id: message.chat.id, text: "Error")
