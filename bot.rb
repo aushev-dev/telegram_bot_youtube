ad4df398585df613d89637dac4555da6078c4593
Mair <aushev92@gmail.com>
2021-02-13 23:44:57 +0300

    change quality audio

--- bot.rb
+++ bot.rb
@@ -9,7 +9,7 @@ Telegram::Bot::Client.run(token) do |bot|
 			if user_list.include?(message.chat.id.to_s)
 				case message.text
 				when /youtu/
-					%x(/app/youtube-dl.dms -x --audio-format mp3 --audio-quality 8 --output "/tmp/audio.%(ext)s" #{message.text})
+					%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
 					bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
 				when //
 					bot.api.send_message(chat_id: message.chat.id, text: message.text)
