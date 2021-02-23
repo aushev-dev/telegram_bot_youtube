d09e7a2ffba83acdca24c736c4d80ebecb4daac8
Mair <aushev92@gmail.com>
2021-02-23 23:25:16 +0300

    delete audio 2

--- bot.rb
+++ bot.rb
@@ -11,7 +11,7 @@ Telegram::Bot::Client.run(token) do |bot|
 				when /youtu/
 					%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
 					bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
-          %x(rm "/tmp/audio.%(ext)s")
+          %x(rm /tmp/audio.mp3)
 				when //
 					bot.api.send_message(chat_id: message.chat.id, text: message.text)
 				end
