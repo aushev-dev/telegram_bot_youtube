f5b361c917bb0f50c7249c80fbbfd423c9501bfb
Mair <aushev92@gmail.com>
2021-10-31 00:51:11 +0300

    added tiktok downloading

--- bot.rb
+++ bot.rb
@@ -12,6 +12,15 @@ Telegram::Bot::Client.run(token) do |bot|
 					%x(/app/youtube-dl.dms -x --audio-format mp3 --output "/tmp/audio.%(ext)s" #{message.text})
 					bot.api.send_audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/tmp/audio.mp3', 'audio/mp3'))
           %x(rm /tmp/audio.mp3)
+        when /tiktok/
+          file = %x(tiktok-scraper video #{message.text} -d -w --filepath /tmp)
+          file_name = file.split('/').last.strip
+          file_path = "/tmp/#{file_name}"
+          if File.exist?(file_path)
+						bot.api.send_video(chat_id: message.chat.id, video: Faraday::UploadIO.new(file_path, 'video/mp4'))
+          else
+						bot.api.send_message(chat_id: message.chat.id, text: "Nothing to send and error #{file}")
+					end
 				when //
 					bot.api.send_message(chat_id: message.chat.id, text: message.text)
 				end
